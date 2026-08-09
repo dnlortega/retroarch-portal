3f9ec31ac2263ef96371d089337627096159379f
dnlortega <dnlortega@gmail.com>
2026-08-09 01:19:11 -0300

    fix: remoção do ssr false no next/dynamic que causava erro no server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ import { AboutSection } from "@/components/AboutSection";
 import { FeaturesSection } from "@/components/FeaturesSection";
 import { Footer } from "@/components/Footer";
 
-const ThemesGallery = dynamic(() => import("@/components/ThemesGallery").then((mod) => mod.ThemesGallery), { ssr: true });
-const HardwareAnalyzer = dynamic(() => import("@/components/HardwareAnalyzer").then((mod) => mod.HardwareAnalyzer), { ssr: false });
-const ConfigBuilder = dynamic(() => import("@/components/ConfigBuilder").then((mod) => mod.ConfigBuilder), { ssr: false });
-const CheatBuilder = dynamic(() => import("@/components/CheatBuilder").then((mod) => mod.CheatBuilder), { ssr: false });
-const RetroDictionary = dynamic(() => import("@/components/RetroDictionary").then((mod) => mod.RetroDictionary), { ssr: true });
-const TipsSection = dynamic(() => import("@/components/TipsSection").then((mod) => mod.TipsSection), { ssr: true });
-const CoresSection = dynamic(() => import("@/components/CoresSection").then((mod) => mod.CoresSection), { ssr: true });
+const ThemesGallery = dynamic(() => import("@/components/ThemesGallery").then((mod) => mod.ThemesGallery));
+const HardwareAnalyzer = dynamic(() => import("@/components/HardwareAnalyzer").then((mod) => mod.HardwareAnalyzer));
+const ConfigBuilder = dynamic(() => import("@/components/ConfigBuilder").then((mod) => mod.ConfigBuilder));
+const CheatBuilder = dynamic(() => import("@/components/CheatBuilder").then((mod) => mod.CheatBuilder));
+const RetroDictionary = dynamic(() => import("@/components/RetroDictionary").then((mod) => mod.RetroDictionary));
+const TipsSection = dynamic(() => import("@/components/TipsSection").then((mod) => mod.TipsSection));
+const CoresSection = dynamic(() => import("@/components/CoresSection").then((mod) => mod.CoresSection));
 
 export default function Home() {
   return (
